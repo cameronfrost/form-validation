b3df3700288d2e9de144800e18ac2b5afa45faa2
Cameron <c@cameronfrost.com>
2021-01-06 19:06:21 -0600

    Query selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("#form");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const passwordConfirmInput = document.querySelector("#password-confirmation");
+const termsInput = document.querySelector("#terms");
+const errorsContainer = document.querySelector(".errors");
+const errorsList = document.querySelector(".errors-list");
